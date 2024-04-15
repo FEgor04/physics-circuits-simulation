e39289c7df062018da96f13c93924f139fdf182d
Egor Fedorov <fegor2004@gmail.com>
2024-04-15 19:10:06 +0300

    set up tanstack router

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,11 +1,20 @@
 // eslint-disable-next-line @conarti/feature-sliced/public-api
+import { RouterProvider, createRouter } from "@tanstack/react-router";
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import { Button } from "@/shared/ui/button";
+import { routeTree } from "@/routeTree.gen";
+
+const router = createRouter({ routeTree })
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <Button>Hi</Button>
+    <RouterProvider router={router} />
   </React.StrictMode>,
 );
