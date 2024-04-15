0291f0c810a45f8278aafbe72ea5bca49a80890c
Egor Fedorov <fegor2004@gmail.com>
2024-04-15 18:53:27 +0300

    install shadcn/ui

--- frontend/src/app/index.tsx
+++ frontend/src/app/index.tsx
@@ -1,9 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
+import { Button } from "@/shared/ui/button";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <h1>hello there!</h1>
+    <Button>
+      Hi
+    </Button>
   </React.StrictMode>,
 );
--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./scr")
+      "@": path.resolve(__dirname, "./src")
     }
   }
 })
