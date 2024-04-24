2f7c0c8dad7f6341e42599ded599679972955edf
Egor Fedorov <fegor2004@gmail.com>
2024-04-24 23:13:38 +0300

    docs: improve circuit simulator docs

--- frontend/src/shared/simulation/interface.ts
+++ frontend/src/shared/simulation/interface.ts
@@ -1,6 +1,20 @@
 import { ElectricalComponent } from "./types.ts"
 
 export interface CircuitSimulator {
+  /**
+   * Добавляет компонент в симуляцию.
+   * Если такой компонент уже существует, то заменяет его новым.
+   **/
   addComponent(component: ElectricalComponent): void
   deleteComponent(component: ElectricalComponent): void
+
+  /**
+   * Возвращает все компоненты в симуляции
+   **/
+  getAllComponents(): Array<ElectricalComponent>
+
+  /**
+   * Заменяет все компоненты переданными
+   **/
+  setComponents(components: Array<ElectricalComponent>): void
 }
