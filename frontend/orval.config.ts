43e6f75f327e45d9eb7fe17274ed61b83beb1e97
Egor Fedorov <fegor2004@gmail.com>
2024-04-25 14:31:43 +0300

    feat: set up orval to generate API

---
+++ frontend/orval.config.ts
@@ -0,0 +1,16 @@
+ import { defineConfig } from 'orval';
+
+ export default defineConfig({
+   backend: {
+     input: {
+       target: "http://localhost:8080/v3/api-docs",
+     },
+     output: {
+       target: "index.ts",
+       client: "axios-functions",
+       workspace: "src/shared/api",
+       mode: "split",
+       mock: true,
+     }
+   }
+ });
