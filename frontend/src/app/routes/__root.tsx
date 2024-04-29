3167697e67a5b398a3c2228ffddf6750cc848982
Egor Fedorov <fegor2004@gmail.com>
2024-04-29 23:31:54 +0300

    style: fix eslint errors

--- frontend/src/app/routes/__root.tsx
+++ frontend/src/app/routes/__root.tsx
@@ -1,6 +1,6 @@
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { createRootRoute, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { Header } from "@/widgets/header";
 
 export const Route = createRootRoute({
