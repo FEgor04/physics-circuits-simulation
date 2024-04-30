4be8f1493cef624d2be92833b715ad03a4a650b2
Egor Fedorov <fegor2004@gmail.com>
2024-04-30 14:18:39 +0300

    tests: add test for transform function

--- frontend/src/widgets/canvas/ui/canvas.tsx
+++ frontend/src/widgets/canvas/ui/canvas.tsx
@@ -1,5 +1,5 @@
-import { ElectricalComponent } from "@/shared/simulation";
 import { useEffect, useRef, useState } from "react";
+import { ElectricalComponent } from "@/shared/simulation";
 import { schemeHeight, schemeWidth, transformVirtualToCanvas } from "../lib";
 
 type Props = {
