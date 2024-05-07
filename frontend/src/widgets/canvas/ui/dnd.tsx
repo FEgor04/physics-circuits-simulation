2c9e4f785510876b16c828468253c40d877b8c78
Egor Fedorov <fegor2004@gmail.com>
2024-05-07 19:41:32 +0300

    feat: add grid snap

--- frontend/src/widgets/canvas/ui/dnd.tsx
+++ frontend/src/widgets/canvas/ui/dnd.tsx
@@ -1,5 +1,18 @@
-import { DndContext } from "@dnd-kit/core";
+import { DndContext, Modifier } from "@dnd-kit/core";
+import { schemeHeight, schemeWidth } from "../lib";
+import { useCanvasParams } from "./context";
 
 export function CanvasDndContext({ children }: React.PropsWithChildren) {
-  return <DndContext>{children}</DndContext>;
+  const params = useCanvasParams();
+  const gridSizeX = params.width / schemeWidth;
+  const gridSizeY = params.height / schemeHeight;
+  const snapToGridModifier: Modifier = (args) => {
+    const { transform } = args;
+    return {
+      ...transform,
+      x: Math.ceil(transform.x / gridSizeX) * gridSizeX,
+      y: Math.ceil(transform.y / gridSizeY) * gridSizeY,
+    }
+  }
+  return <DndContext modifiers={[snapToGridModifier]}>{children}</DndContext>;
 }
