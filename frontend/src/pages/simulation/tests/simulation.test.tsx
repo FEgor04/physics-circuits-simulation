9a85b4cd240bf256baad0367b29f9187b92d416b
Egor Fedorov <fegor2004@gmail.com>
2024-05-09 16:07:32 +0300

    tests: add test for simulation page

--- frontend/src/pages/simulation/tests/simulation.test.tsx
+++ frontend/src/pages/simulation/tests/simulation.test.tsx
@@ -1,17 +1,23 @@
 import { render, screen } from "@testing-library/react";
-import { describe, expect, test as it } from "vitest";
+import { describe, expect, test as it, vi } from "vitest";
 import { Simulation } from "../ui";
+
+global.ResizeObserver = vi.fn().mockImplementation(() => ({
+  observe: vi.fn(),
+  unobserve: vi.fn(),
+  disconnect: vi.fn(),
+}));
 
 describe("simulation page", () => {
   it("should render canvas & addComponent menu in editing mode", () => {
     render(<Simulation mode="editing" setMode={console.log} />);
-    expect(screen.getByTestId("components-choose-bar")).not.toBeUndefined();
-    expect(screen.getByTestId("components-canvas")).not.toBeUndefined();
+    expect(screen.queryByTestId("components-choose-bar")).not.toBeNull();
+    expect(screen.queryByTestId("components-canvas")).not.toBeNull();
   });
 
   it("should not render addComponent menu in simulatino mode", () => {
     render(<Simulation mode="simulation" setMode={console.log} />);
-    expect(screen.getByTestId("components-choose-bar")).toBeUndefined();
-    expect(screen.getByTestId("components-canvas")).not.toBeUndefined();
-  })
+    expect(screen.queryByTestId("components-choose-bar")).toBeNull();
+    expect(screen.queryByTestId("components-canvas")).not.toBeNull();
+  });
 });
