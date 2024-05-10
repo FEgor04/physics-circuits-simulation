d9059692d6d6124f1c3bab8ae45a6c29bec00605
Egor Fedorov <fegor2004@gmail.com>
2024-05-10 21:00:14 +0300

    refactor: use data-selected attribute to set fill color

--- frontend/src/widgets/canvas/ui/tests/canvas.test.tsx
+++ frontend/src/widgets/canvas/ui/tests/canvas.test.tsx
@@ -3,8 +3,13 @@ import userEvent from "@testing-library/user-event";
 import { describe, expect, test as it, vi } from "vitest";
 import { SelectComponentProvider } from "@/features/select-component";
 import { Canvas } from "../canvas";
+import { beforeEach } from "node:test";
 
 describe("canvas", () => {
+  beforeEach(() => {
+    vi.resetAllMocks();
+  });
+
   it("select dot after click", async () => {
     const onSelect = vi.fn().mockImplementation(console.log);
     render(
@@ -21,6 +26,23 @@ describe("canvas", () => {
 
     await userEvent.click(screen.getByTestId("dot-1-1"));
     expect(onSelect).toHaveBeenCalledOnce();
+  });
+
+  it("renders selected dot with data-selected = true", async () => {
+    const onSelect = vi.fn().mockImplementation(console.log);
+    render(
+      <SelectComponentProvider selected={{ type: "point", point: { x: 1, y: 1 } }} onSelect={onSelect}>
+        <Canvas
+          components={[]}
+          onAddComponent={console.log}
+          onUpdateComponent={console.log}
+          onUpdateComponentCoords={console.log}
+          canvasSize={100}
+        />
+      </SelectComponentProvider>,
+    );
+
+    expect(screen.getByTestId("dot-1-1-visual").getAttribute("data-selected")).toBe("true");
   });
 
   // Will be faced in issue BIPKI-67
