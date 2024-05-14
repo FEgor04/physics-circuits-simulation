d96f853412cd22a12f860d7185514a0b12e9b613
Egor Fedorov <fegor2004@gmail.com>
2024-05-14 09:13:24 +0300

    feat(update-component): add UpdateSourceDC

---
+++ frontend/src/features/update-component/ui/source-dc.tsx
@@ -0,0 +1,46 @@
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { WithID, SourceDC } from "@/shared/simulation";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "@/shared/ui/form";
+import { Input } from "@/shared/ui/input";
+import { useOnUpdateComponent } from "../model/hooks";
+
+type Props = {
+  defaultValue: WithID<SourceDC>;
+};
+
+const schema = z.object({
+  electromotiveForce: z.coerce.number(),
+});
+
+export function UpdateSourceDC({ defaultValue }: Props) {
+  const onUpdate = useOnUpdateComponent();
+  const form = useForm<z.infer<typeof schema>>({
+    resolver: zodResolver(schema),
+    defaultValues: defaultValue,
+  });
+
+  function onSubmit(values: z.infer<typeof schema>) {
+    onUpdate({ ...defaultValue, ...values });
+  }
+
+  return (
+    <Form {...form}>
+      <form id="update-source-dc" onSubmit={form.handleSubmit(onSubmit)}>
+        <FormField
+          control={form.control}
+          name="electromotiveForce"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Электродвижущая сила</FormLabel>
+              <FormControl>
+                <Input {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
+}
---
+++ frontend/src/features/update-component/ui/tests/source-dc.test.tsx
@@ -0,0 +1,86 @@
+import { render, screen, waitFor } from "@testing-library/react";
+import { userEvent } from "@testing-library/user-event";
+import { describe, it, expect, vi, beforeEach } from "vitest";
+import { WithID } from "@/shared/simulation";
+import { SourceDC } from "@/shared/simulation/types";
+import { UpdateComponentProvider } from "../../model/provider";
+import { UpdateSourceDC } from "../source-dc";
+
+function createSourceDC(electromotiveForce: number = 100): WithID<SourceDC> {
+  return {
+    id: 1,
+    _type: "sourceDC",
+    plus: { x: 0, y: 0 },
+    minus: { x: 1, y: 0 },
+    electromotiveForce,
+  };
+}
+
+const setup = () => {
+  const user = userEvent.setup();
+  const ef = 420;
+  const onUpdateComponent = vi.fn().mockImplementation(console.log);
+  const utils = render(
+    <UpdateComponentProvider onUpdateComponent={onUpdateComponent}>
+      <UpdateSourceDC defaultValue={createSourceDC(ef)} />
+      <button type="submit" form="update-source-dc">
+        Submit
+      </button>
+    </UpdateComponentProvider>,
+  );
+  const input = screen.getByLabelText("Электродвижущая сила");
+  const submit = screen.getByText("Submit");
+  const label = screen.getByText("Электродвижущая сила");
+  return { ...utils, onUpdateComponent, ef, input, user, submit, label };
+};
+
+describe("Update Source DC Form", () => {
+  beforeEach(() => {
+    vi.resetAllMocks();
+  });
+
+  it("should render defaultValue", () => {
+    const { input, ef } = setup();
+    expect(input).toHaveProperty("value", String(ef));
+  });
+
+  it("should allow to enter positive electromotiveForce", async () => {
+    const { input, onUpdateComponent, user, submit, label } = setup();
+
+    await waitFor(() => user.clear(input));
+    await waitFor(() => user.type(input, "228"));
+    await waitFor(() => user.click(submit));
+    expect(label.className).not.toMatch("text-destructive");
+    expect(onUpdateComponent).toHaveBeenCalledWith(createSourceDC(228));
+  });
+
+  it("should allow to enter negative electromotiveForce", async () => {
+    const { input, onUpdateComponent, user, submit, label } = setup();
+
+    await waitFor(() => user.clear(input));
+    await waitFor(() => user.type(input, "-228"));
+    await waitFor(() => user.click(submit));
+    expect(label.className).not.toMatch("text-destructive");
+    expect(onUpdateComponent).toHaveBeenCalledWith(createSourceDC(-228));
+  });
+
+  it("should not allow to enter electromotiveForce with letters", async () => {
+    const { input, onUpdateComponent, user, submit, label } = setup();
+
+    await waitFor(() => user.clear(input));
+    await waitFor(() => user.type(input, "asd321das"));
+    await waitFor(() => user.click(submit));
+    expect(label.className).toMatch("text-destructive");
+    expect(onUpdateComponent).toHaveBeenCalledTimes(0);
+  });
+
+  it("should not allow to enter electromotiveForce with letters on the end", async () => {
+    const { input, onUpdateComponent, user, submit, label } = setup();
+
+    await waitFor(() => user.clear(input));
+    await waitFor(() => user.type(input, "321as"));
+    await waitFor(() => user.click(submit));
+    expect(label.className).toMatch("text-destructive");
+    expect(onUpdateComponent).toHaveBeenCalledTimes(0);
+  });
+});
