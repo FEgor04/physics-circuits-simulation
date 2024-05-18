5c5515a3359c98ce0b69875016c9ed0620772bf1
Egor Fedorov <fegor2004@gmail.com>
2024-05-18 09:59:00 +0300

    feat: add /schemes page

---
+++ frontend/src/entities/scheme/index.ts
@@ -0,0 +1,7 @@
+export * from "./model/scheme";
+export * from "./model/component";
+
+export * from "./api/get-scheme";
+export * from "./api/get-schemes";
+export * from "./api/create-scheme";
+export * from "./api/update-scheme";
---
+++ frontend/src/pages/schemes/ui/page.tsx
@@ -0,0 +1,9 @@
+import { Scheme } from "@/entities/scheme";
+
+type Props = {
+  schemes: Array<Scheme>;
+};
+
+export function SchemesPage({ schemes }: Props) {
+  return <div className="grid grid-cols-1 gap-4 md:grid-cols-2 lg:grid-cols-4"></div>;
+}
