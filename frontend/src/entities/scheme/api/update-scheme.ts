849822e36f476997fee54d4cd3fc6d2db43dbdb6
Egor Fedorov <fegor2004@gmail.com>
2024-05-19 12:24:42 +0300

    fix mutation invalidations

--- frontend/src/entities/scheme/api/update-scheme.ts
+++ frontend/src/entities/scheme/api/update-scheme.ts
@@ -1,7 +1,6 @@
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { updateScheme } from "@/shared/api";
 import { ElectricalComponentWithID } from "@/shared/simulation";
-import { getSchemeByIDQueryOptions } from "./get-scheme";
 import { componentToDTO } from "./transform";
 
 export function useUpdateSchemeMutation() {
@@ -11,8 +10,8 @@ export function useUpdateSchemeMutation() {
       const data = await updateScheme(params.id, params.components.map(componentToDTO));
       return data.data;
     },
-    onSuccess: (_, { id }) => {
-      queryClient.invalidateQueries(getSchemeByIDQueryOptions(id));
+    onSuccess: () => {
+      queryClient.invalidateQueries({ queryKey: ["schemes"] });
     },
   });
 }
