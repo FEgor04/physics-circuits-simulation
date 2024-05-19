fcae336bc6067205a0e5e037b436f572192039aa
Egor Fedorov <fegor2004@gmail.com>
2024-05-19 12:45:25 +0300

    style: fix eslint errors

--- frontend/src/app/routes/index.tsx
+++ frontend/src/app/routes/index.tsx
@@ -7,7 +7,10 @@ export const Route = createFileRoute("/")({
 function Component() {
   return (
     <div className="container mx-auto mt-8">
-      <h1 className="font-bold text-center text-4xl md:text-5xl lg:text-6xl"> <span className="underline text-primary">Азат</span>, сделай пожалуйста красивый лендинг!!! </h1>
+      <h1 className="text-center text-4xl font-bold md:text-5xl lg:text-6xl">
+        {" "}
+        <span className="text-primary underline">Азат</span>, сделай пожалуйста красивый лендинг!!!{" "}
+      </h1>
     </div>
   );
 }
