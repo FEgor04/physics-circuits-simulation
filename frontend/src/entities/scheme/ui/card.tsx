768678db479d828b7938b44299d079e62848683a
Egor Fedorov <fegor2004@gmail.com>
2024-05-19 12:29:29 +0300

    fix DOM nesting error

--- frontend/src/entities/scheme/ui/card.tsx
+++ frontend/src/entities/scheme/ui/card.tsx
@@ -1,3 +1,4 @@
+import { Link } from "@tanstack/react-router";
 import { Card, CardDescription, CardHeader, CardTitle } from "@/shared/ui/card";
 import { Skeleton } from "@/shared/ui/skeleton";
 import { Scheme } from "../model/scheme";
@@ -11,9 +12,7 @@ export function SchemeCard({ scheme, tooltip }: { scheme: Scheme; tooltip: React
       <CardHeader className="flex flex-row items-start justify-between">
         <div>
           <CardTitle className="text-lg">{scheme.name}</CardTitle>
-          <CardDescription className="flex flex-row justify-between">
-            <p>{scheme.authorName}</p>
-          </CardDescription>
+          <CardDescription className="flex flex-row justify-between">{scheme.authorName}</CardDescription>
         </div>
         <div>{tooltip}</div>
       </CardHeader>
