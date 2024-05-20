f88848c2972aaee11d0aa8d4c579f9e31f7ba229
Egor Fedorov <fegor2004@gmail.com>
2024-05-20 10:00:52 +0300

    fix: remove second generic

--- frontend/src/shared/api/instance.ts
+++ frontend/src/shared/api/instance.ts
@@ -3,10 +3,10 @@ import Axios, { AxiosRequestConfig } from 'axios';
 
 export const AXIOS_INSTANCE = axios.create();
 
- export const customInstance = <T, E>(
+ export const customInstance = <T>(
    config: AxiosRequestConfig,
    options?: AxiosRequestConfig,
- ): Promise<AxiosResponse<T, E>> => {
+ ): Promise<AxiosResponse<T>> => {
    const source = Axios.CancelToken.source();
    const promise = AXIOS_INSTANCE<T>({
      ...config,
