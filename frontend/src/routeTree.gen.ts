5b08f914bd37b37ca466b4e845a7a14994862940
Egor Fedorov <fegor2004@gmail.com>
2024-05-20 11:04:27 +0300

    fix  compilation error

--- frontend/src/routeTree.gen.ts
+++ frontend/src/routeTree.gen.ts
@@ -16,7 +16,6 @@ import { Route as SigninImport } from "./app/routes/signin";
 import { Route as EmbedImport } from "./app/routes/embed";
 import { Route as AuthenticatedRouteImport } from "./app/routes/_authenticated/route";
 import { Route as IndexImport } from "./app/routes/index";
-import { Route as AuthenticatedSchemesRouteImport } from "./app/routes/_authenticated/schemes.route";
 import { Route as AuthenticatedSchemesIndexImport } from "./app/routes/_authenticated/schemes.index";
 import { Route as AuthenticatedSchemesSchemeImport } from "./app/routes/_authenticated/schemes.$scheme";
 
@@ -47,20 +46,15 @@ const IndexRoute = IndexImport.update({
   getParentRoute: () => rootRoute,
 } as any);
 
-const AuthenticatedSchemesRouteRoute = AuthenticatedSchemesRouteImport.update({
-  path: "/schemes",
-  getParentRoute: () => AuthenticatedRouteRoute,
-} as any);
-
 const AuthenticatedSchemesIndexRoute = AuthenticatedSchemesIndexImport.update({
-  path: "/",
-  getParentRoute: () => AuthenticatedSchemesRouteRoute,
+  path: "/schemes/",
+  getParentRoute: () => AuthenticatedRouteRoute,
 } as any);
 
 const AuthenticatedSchemesSchemeRoute = AuthenticatedSchemesSchemeImport.update(
   {
-    path: "/$scheme",
-    getParentRoute: () => AuthenticatedSchemesRouteRoute,
+    path: "/schemes/$scheme",
+    getParentRoute: () => AuthenticatedRouteRoute,
   } as any,
 );
 
@@ -69,53 +63,68 @@ const AuthenticatedSchemesSchemeRoute = AuthenticatedSchemesSchemeImport.update(
 declare module "@tanstack/react-router" {
   interface FileRoutesByPath {
     "/": {
+      id: "/";
+      path: "/";
+      fullPath: "/";
       preLoaderRoute: typeof IndexImport;
       parentRoute: typeof rootRoute;
     };
     "/_authenticated": {
+      id: "/_authenticated";
+      path: "";
+      fullPath: "";
       preLoaderRoute: typeof AuthenticatedRouteImport;
       parentRoute: typeof rootRoute;
     };
     "/embed": {
+      id: "/embed";
+      path: "/embed";
+      fullPath: "/embed";
       preLoaderRoute: typeof EmbedImport;
       parentRoute: typeof rootRoute;
     };
     "/signin": {
+      id: "/signin";
+      path: "/signin";
+      fullPath: "/signin";
       preLoaderRoute: typeof SigninImport;
       parentRoute: typeof rootRoute;
     };
     "/signup": {
+      id: "/signup";
+      path: "/signup";
+      fullPath: "/signup";
       preLoaderRoute: typeof SignupImport;
       parentRoute: typeof rootRoute;
     };
-    "/_authenticated/schemes": {
-      preLoaderRoute: typeof AuthenticatedSchemesRouteImport;
+    "/_authenticated/schemes/$scheme": {
+      id: "/_authenticated/schemes/$scheme";
+      path: "/schemes/$scheme";
+      fullPath: "/schemes/$scheme";
+      preLoaderRoute: typeof AuthenticatedSchemesSchemeImport;
       parentRoute: typeof AuthenticatedRouteImport;
     };
-    "/_authenticated/schemes/$scheme": {
-      preLoaderRoute: typeof AuthenticatedSchemesSchemeImport;
-      parentRoute: typeof AuthenticatedSchemesRouteImport;
-    };
     "/_authenticated/schemes/": {
+      id: "/_authenticated/schemes/";
+      path: "/schemes";
+      fullPath: "/schemes";
       preLoaderRoute: typeof AuthenticatedSchemesIndexImport;
-      parentRoute: typeof AuthenticatedSchemesRouteImport;
+      parentRoute: typeof AuthenticatedRouteImport;
     };
   }
 }
 
 // Create and export the route tree
 
-export const routeTree = rootRoute.addChildren([
+export const routeTree = rootRoute.addChildren({
   IndexRoute,
-  AuthenticatedRouteRoute.addChildren([
-    AuthenticatedSchemesRouteRoute.addChildren([
-      AuthenticatedSchemesSchemeRoute,
-      AuthenticatedSchemesIndexRoute,
-    ]),
-  ]),
+  AuthenticatedRouteRoute: AuthenticatedRouteRoute.addChildren({
+    AuthenticatedSchemesSchemeRoute,
+    AuthenticatedSchemesIndexRoute,
+  }),
   EmbedRoute,
   SigninRoute,
   SignupRoute,
-]);
+});
 
 /* prettier-ignore-end */
