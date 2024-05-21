45b88b2d75a84f72a61547b2e02ec80c16a73520
Azat222 <azat222@gmail.com>
2024-05-22 02:54:15 +0300

    feat: add link

--- frontend/src/app/routes/index.tsx
+++ frontend/src/app/routes/index.tsx
@@ -11,6 +11,13 @@ function Component() {
         {" "}
         <span className="text-primary underline">Азат</span>, сделай пожалуйста красивый лендинг!!!{" "}
       </h1>
+      <br />
+      <h2 className="text-center text-4xl font-bold">
+        <span className="text-primary underline">Похуй</span> на лендинг от{" "}
+        <a className="text-blue-400 underline" href={"https://se.ifmo.ru/~s368796/"}>
+          Обучающей статьи хоть кайфуйте
+        </a>
+      </h2>
     </div>
   );
 }
