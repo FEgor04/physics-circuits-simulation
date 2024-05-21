88abf4305adacd5fcac4d9f7bce6af2b4a9f7a92
Egor Fedorov <fegor2004@gmail.com>
2024-05-21 14:03:27 +0300

    docs(Event): add `discriminatorMapping` & `discriminatorProperties`

--- backend/src/main/java/org/circuisim/rSocket/data/Event.java
+++ backend/src/main/java/org/circuisim/rSocket/data/Event.java
@@ -1,5 +1,6 @@
 package org.circuisim.rSocket.data;
 
+import io.swagger.v3.oas.annotations.media.DiscriminatorMapping;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
 import org.circuisim.web.dto.ElectricalComponentDto;
@@ -8,13 +9,19 @@ import java.io.Serializable;
 import java.util.List;
 
 @Data
-@Schema(description = "Event",oneOf = {
+@Schema(description = "Event", oneOf = {
         DeleteComponentsEvent.class,
         UpdateComponentEvent.class,
         UserSelectedEvent.class,
         UserConnectedEvent.class,
         UserDisconnectedEvent.class,
-})
+}, discriminatorMapping = {
+        @DiscriminatorMapping(schema = DeleteComponentsEvent.class, value = "DELETE"),
+        @DiscriminatorMapping(schema = UpdateComponentEvent.class, value = "UPDATE"),
+        @DiscriminatorMapping(schema = UserSelectedEvent.class, value = "SELECT"),
+        @DiscriminatorMapping(schema = UserConnectedEvent.class, value = "CONNECT"),
+        @DiscriminatorMapping(schema = UserDisconnectedEvent.class, value = "DISCONNECT"),
+}, discriminatorProperty = "type")
 public sealed class Event implements Serializable permits DeleteComponentsEvent,
         UpdateComponentEvent,
         UserSelectedEvent,
