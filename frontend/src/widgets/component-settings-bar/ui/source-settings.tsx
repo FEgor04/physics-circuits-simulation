db5d5d1f51908ca9f6fed5d32e98555762cc307b
Azat222 <azat222@gmail.com>
2024-05-22 22:06:43 +0300

    fix: change name of dc source

--- frontend/src/widgets/component-settings-bar/ui/source-settings.tsx
+++ frontend/src/widgets/component-settings-bar/ui/source-settings.tsx
@@ -8,7 +8,7 @@ export function SourceSettings({ component }: { component: WithID<Source> }) {
   const onDeleteComponent = useDeleteComponent();
   return (
     <div className="flex w-full flex-col gap-2 p-4">
-      <p>Резистор</p>
+      <p>Источник постоянного тока</p>
       <UpdateSource defaultValue={component} />
       <div className="flex flex-row items-center space-x-2">
         <Button type="submit" form="update-source">
