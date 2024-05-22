309d9d49992983d4ec3a1f296dbb32286df4ad96
Egor Fedorov <fegor2004@gmail.com>
2024-05-22 15:31:32 +0300

    tests: add test for JWT refresh

--- backend/src/test/java/org/circuisim/web/controller/AuthControllerTest.java
+++ backend/src/test/java/org/circuisim/web/controller/AuthControllerTest.java
@@ -1,6 +1,10 @@
 package org.circuisim.web.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.circuisim.service.AuthService;
+import org.circuisim.service.UserService;
+import org.circuisim.web.dto.UserDto;
+import org.circuisim.web.dto.auth.JwtRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -22,6 +26,10 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 class AuthControllerTest {
     @Autowired
     private MockMvc mockMvc;
+    @Autowired
+    private UserService userService;
+    @Autowired
+    private AuthService authService;
 
     @Test
     void refreshNoJwtTokenShouldReturnUnauthorized() throws Exception {
@@ -33,13 +41,20 @@ class AuthControllerTest {
         mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/refresh").content("eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzdXBlcm1hbkBnbWFpbC5jb20iLCJpZCI6MSwicm9sZXMiOlsiQURNSU4iXSwiaWF0IjoxNzE2MzA5NjExLCJleHAiOjE3MTYzMTMyMTF9.E8BMSqQBU2qjlZXTaMu47iV0pTEXEcX2_NzqQQO7dODLNWOcfFxdgJEuvVZxia3Kr2lgOuxr8fg_y0A4r9x1e111w").header("Authorization", "Bearer " + "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzdXBlcm1hbkBnbWFpbC5jb20iLCJpZCI6MSwicm9sZXMiOlsiQURNSU4iXSwiaWF0IjoxNzE2MzA5NjExLCJleHAiOjE3MTYzMTMyMTF9.E8BMSqQBU2qjlZXTaMu47iV0pTEXEcX2_NzqQQO7dODLNWOcfFxdgJEuvVZxia3Kr2lgOuxr8fg_y0A4r9x1e111w")).andDo(print()).andExpect(status().is(401));
     }
 
+
     @Test
-    void refreshBadJwtRefreshTokenShouldReturnUnauthorized() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/refresh").content("eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzdXBlcm1hbkBnbWFpbC5jb20iLCJpZCI6MSwicm9sZXMiOlsiQURNSU4iXSwiaWF0IjoxNzE2MzA5NjExLCJleHAiOjE3MTYzMTMyMTF9.E8BMSqQBU2qjlZXTaMu47iV0pTEXEcX2_NzqQQO7dODLNWOcfFxdgJEuvVZxia3Kr2lgOuxr8fg_y0A4r9x1e123w").header("Authorization", "Bearer " + "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzdXBlcm1hbkBnbWFpbC5jb20iLCJpZCI6MSwicm9sZXMiOlsiQURNSU4iXSwiaWF0IjoxNzE2MzA5NjExLCJleHAiOjE3MTYzMTMyMTF9.E8BMSqQBU2qjlZXTaMu47iV0pTEXEcX2_NzqQQO7dODLNWOcfFxdgJEuvVZxia3Kr2lgOuxr8fg_y0A4r9x1e111w")).andDo(print()).andExpect(status().is(401));
+    void refreshWithoutAccessTokenWithBadRefreshTokenShouldReturnUnauthorized() throws Exception {
+        var refreshToken = "eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzdXBlcm1hbkBnbWFpbC5jb20iLCJpZCI6MSwicm9sZXMiOlsiQURNSU4iXSwiaWF0IjoxNzE2MzA5NjExLCJleHAiOjE3MTYzMTMyMTF9.E8BMSqQBU2qjlZXTaMu47iV0pTEXEcX2_NzqQQO7dODLNWOcfFxdgJEuvVZxia3Kr2lgOuxr8fg_y0A4r9x1e123w";
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/refresh").content(refreshToken))
+                .andDo(print())
+                .andExpect(status().is(401));
     }
 
     @Test
-    void refreshWithoutAccessTokenShouldReturnUnauthorized() throws Exception {
-        mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/refresh").content("eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJzdXBlcm1hbkBnbWFpbC5jb20iLCJpZCI6MSwicm9sZXMiOlsiQURNSU4iXSwiaWF0IjoxNzE2MzA5NjExLCJleHAiOjE3MTYzMTMyMTF9.E8BMSqQBU2qjlZXTaMu47iV0pTEXEcX2_NzqQQO7dODLNWOcfFxdgJEuvVZxia3Kr2lgOuxr8fg_y0A4r9x1e123w")).andDo(print()).andExpect(status().is(401));
+    void refreshWithoutAccessTokenWithGoodRefreshTokenShouldReturnOk() throws Exception {
+        var user = this.userService.create(new UserDto(null, "Test", "test1@test.com", "123456"));
+        var token = this.authService.login(new JwtRequest(user.getEmail(), "123456"));
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/auth/refresh").content(token.getRefreshToken()))
+                .andDo(print()).andExpect(status().isOk());
     }
 }
