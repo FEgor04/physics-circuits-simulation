e37e4013519cadc7cf9ed5b60c1936bc091fedad
forafox <deskpa@yandex.ru>
2024-05-25 04:09:37 +0300

    tests: getSchemeWithEmbeddedMode

--- backend/src/test/java/org/circuisim/web/controller/SchemesControllerTest.java
+++ backend/src/test/java/org/circuisim/web/controller/SchemesControllerTest.java
@@ -1,9 +1,12 @@
 package org.circuisim.web.controller;
 
 import lombok.RequiredArgsConstructor;
+import org.circuisim.domain.Role;
+import org.circuisim.domain.User;
 import org.circuisim.domain.simulation.Scheme;
 import org.circuisim.service.SchemeService;
 import org.circuisim.web.responseRecord.SchemeResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -11,9 +14,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MockMvcBuilder;
+import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -27,13 +35,25 @@ class SchemesControllerTest {
     @Autowired
     private SchemeService schemeService;
 
-    @Test
-    void getEmbeddedSchemeByIdWithoutJWT_shouldReturnScheme() {
-        Scheme scheme = new Scheme(
-
-        )
+    @BeforeEach
+    void setUp() {
+        User mockUser = new User(1L, "test1", "test@example.com", "123", Set.of(Role.USER));
+        Scheme scheme = new Scheme(1L, "Test scheme", false, mockUser, new HashSet<>(), new HashSet<>());
+        Scheme schemeWithEmbeddedStatus = new Scheme(2L, "Test scheme", true, mockUser, new HashSet<>(), new HashSet<>());
         schemeService.save(scheme);
-        SchemeResponse schemeResponse = mockMvc.perform(MockMvcRequestBuilders.
-                get("api/schemes/")
+        schemeService.save(schemeWithEmbeddedStatus);
+    }
+
+
+    @Test
+    void getSchemeByIdWithoutJWT_shouldReturn401Status() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.
+                get("/api/schemes/1")).andExpect(status().is(401));
+    }
+
+    @Test
+    void getEmbeddedSchemeByIdWithoutJWT_shouldReturnScheme() throws Exception {
+        mockMvc.perform(MockMvcRequestBuilders.
+                get("/api/schemes/2")).andExpect(status().is(200));
     }
 }
