f80717eb64c4dfebb266463fb86a715634d23b4c
Egor Fedorov <fegor2004@gmail.com>
2024-05-27 22:16:45 +0300

    feat: add link to index

--- frontend/src/widgets/header/ui/index.tsx
+++ frontend/src/widgets/header/ui/index.tsx
@@ -1,4 +1,6 @@
+import { useQuery } from "@tanstack/react-query";
 import { Link } from "@tanstack/react-router";
+import { getMeQueryOptions } from "@/entities/principal";
 import { Button } from "@/shared/ui/button.tsx";
 
 type Props = {
@@ -9,14 +11,32 @@ export function Header({ principalMenu }: Props) {
   return (
     <header className="border-b-4 bg-white">
       <nav className="container mx-auto flex h-12 flex-row items-center justify-between">
-        <Link to="/schemes" className="font-bold text-black md:text-2xl">
+        <MainLink>
+          <span className="font-bold text-black md:text-2xl">
           circui<span className="text-primary">sim</span>
-        </Link>
+          </span>
+        </MainLink>
         <Button variant={"link"} asChild>
           <Link to="/article">Обучающая статья</Link>
         </Button>
         {principalMenu}
       </nav>
     </header>
+  );
+}
+
+function MainLink({ children }: React.PropsWithChildren) {
+  const { data } = useQuery(getMeQueryOptions());
+  if (data) {
+    return (
+      <Link to="/schemes" className="">
+        {children}
+      </Link>
+    );
+  }
+  return (
+    <Link to="/" className="">
+      {children}
+    </Link>
   );
 }
